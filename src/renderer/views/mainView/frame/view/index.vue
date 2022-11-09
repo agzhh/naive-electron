f68f53e43d8667e469761443a480eb21ef98fff8
敛馀辉 <agzhh@qq.com>
2022-11-09 13:11:44 +0800

    feat(components): 完成视图组件开发

--- src/renderer/views/mainView/frame/view/index.vue
+++ src/renderer/views/mainView/frame/view/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>视图</div>
+  <n-button type="info" @click="openBili">打开哔哩哔哩</n-button>
+  <n-button @click="removeBili">移除哔哩哔哩</n-button>
+  <iframe :src="iframeUrl" v-if="iframeUrl" style="width: 100%; height: 100%"></iframe>
 </template>
 
-<script>
-export default {
-  name: 'index'
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const iframeUrl = ref('');
+
+const openBili = () => {
+  iframeUrl.value = 'https://www.bilibili.com/';
+};
+const removeBili = () => {
+  iframeUrl.value = '';
 };
 </script>
 
